268cdd07e69ed55b2cd23fa63af88975a3c57293
IKENNA UDEANI <ikennaudeani@gmail.com>
2022-02-14 16:04:42 +0100

    Add files via upload

---
+++ mslearn-azure-sql-fundamentals/04-Performance/monitor_and_scale/sqlworkload.cmd
@@ -0,0 +1,1 @@
+ostress.exe -Saw-server<unique_id>.database.windows.net -itopcustomersales.sql -Ucloudadmin -dAdventureWorks -P<password> -n10 -r2 -q
---
+++ mslearn-azure-sql-fundamentals/04-Performance/tuning_recommendations/query_order_rating.cmd
@@ -0,0 +1,5 @@
+date /T
+time /T
+ostress.exe -Saw-server<unique_ID>.database.windows.net -iquery_order_rating.sql -Ucloudadmin -dAdventureWorks -P<password> -n25 -r100000 -q
+date /T
+time /T
